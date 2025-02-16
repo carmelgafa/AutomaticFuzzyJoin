38ff44f5f5805b0c5d92a54cc278dbc64af9cdea
Carmel Gafa <carmelgafa@gmail.com>
2025-02-16 20:49:40 +0100

    comments

--- src/fj/levenshtein.py
+++ src/fj/levenshtein.py
@@ -10,7 +10,7 @@ def levenshtein_distance(str_1:str, str_2:str) -> int:
     # Initialize a matrix to store the edit distances
     dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
 
-    # Initialize the first row and column with values from 0 to m and 0 to n respectively
+    # Initialize the first row and column
     for i in range(m + 1):
         dp[i][0] = i
     for j in range(n + 1):
@@ -21,10 +21,10 @@ def levenshtein_distance(str_1:str, str_2:str) -> int:
     for i in range(1, m + 1):
         for j in range(1, n + 1):
             if str_1[i - 1] == str_2[j - 1]:
-                # Characters match, no operation needed
+                # Characters match, good! No extra cost
                 dp[i][j] = dp[i - 1][j - 1]
             else:
-                # Characters don't match, choose minimum cost 
+                # Characters don't match, choose minimum cost
                 # among insertion, deletion, or substitution
                 dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
 
